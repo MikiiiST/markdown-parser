30a4dcd4a9d25a6af3b0c575d2e9c00be0f1af77
MikiiiST <wyszeto@ucsd.edu>
2022-05-06 19:16:59 -0700

    makeFile

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,28 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+
 public class MarkdownParseTest {
 
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testLink() throws IOException{
+        ArrayList<String> test = new ArrayList<>();
+        test.add("https://something.com");
+        test.add("some-thing.html");
+
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+
+        assertEquals(test, MarkdownParse.getLinks(content));
     }
 
 }
