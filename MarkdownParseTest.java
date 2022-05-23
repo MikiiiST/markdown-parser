41b4d5c5581a7a95eca7b74a95b67f30cda489d7
MikiiiST <wyszeto@ucsd.edu>
2022-05-22 21:18:49 -0700

    changedforlab4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -25,4 +25,45 @@ public class MarkdownParseTest {
         assertEquals(test, MarkdownParse.getLinks(content));
     }
 
+    @Test
+    public void testSnippet1() throws IOException{
+        ArrayList<String> test = new ArrayList<>();
+        test.add("url.com");
+        test.add("`google.com");
+        test.add("google.com");
+        test.add("ucsd.edu");
+
+        Path fileName = Path.of("testsforLab7.md");
+        String content = Files.readString(fileName);
+
+        assertEquals(test, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testSnippet2() throws IOException{
+        ArrayList<String> test = new ArrayList<>();
+        test.add("b.com");
+        test.add("a.com(())");
+        test.add("example.com");
+
+        Path fileName = Path.of("snippet2.md");
+        String content = Files.readString(fileName);
+
+        assertEquals(test, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException{
+        ArrayList<String> test = new ArrayList<>();
+        test.add("https://www.twitter.com");
+        test.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+        test.add("github.com");
+        test.add("https://cse.ucsd.edu/");
+
+        Path fileName = Path.of("snippet3.md");
+        String content = Files.readString(fileName);
+
+        assertEquals(test, MarkdownParse.getLinks(content));
+    }
+
 }
